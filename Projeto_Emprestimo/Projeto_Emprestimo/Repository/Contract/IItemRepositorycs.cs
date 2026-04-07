fa236d115d4ff07b08485c0fdceac93c88389c37
unknown <3mtec@e041edu.local>
2026-04-07 09:25:02 -0300

    Criando interface

---
+++ Projeto_Emprestimo/Projeto_Emprestimo/Repository/Contract/IItemRepositorycs.cs
@@ -0,0 +1,15 @@
+﻿using Projeto_Emprestimo.Models;
+
+namespace Projeto_Emprestimo.Repository.Contract
+{
+    public interface IItemRepository
+    {
+
+        //CRUD
+        IEnumerable<Itens> ObterTodosItens();
+        void Cadastrar(Itens item);
+        void Atualizar(Itens item);
+        Itens ObterItens(int Id);
+        void Excluir(int Id);
+    }
+}
